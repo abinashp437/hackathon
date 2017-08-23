05384cd8344ec31d63faceedc6c45c97ce5c4f23
Deepak345 <senapati.dipak97@gmail.com>
2017-08-23 19:32:39 +0530

    added different functionalities

--- Expense Manager/myscript.js
+++ Expense Manager/myscript.js
@@ -1,22 +1,61 @@
 var myLink = document.getElementById('mylink');
 myLink.onclick = function() {
+    update();
+    percentage();
+    excess();
+    document.getElementById("input").value = "";
+}
+
+function percentage() {
+    var fp = (document.getElementById("foodex").innerHTML / document.getElementById("fl").innerHTML) * 100;
+    var tp = (document.getElementById("travelex").innerHTML / document.getElementById("tl").innerHTML) * 100;
+    var cp = (document.getElementById("clothesex").innerHTML / document.getElementById("cl").innerHTML) * 100;
+    var bp = (document.getElementById("billex").innerHTML / document.getElementById("bl").innerHTML) * 100;
+    document.getElementById("fper").innerHTML = fp.toFixed(2);
+    document.getElementById("tper").innerHTML = tp.toFixed(2);
+    document.getElementById("cper").innerHTML = cp.toFixed(2);
+    document.getElementById("bper").innerHTML = bp.toFixed(2);
+}
+
+function update() {
     var e = document.getElementById("myList").value;
-    console.log(e);
     var x = document.getElementById("input").value;
-    if(e=='food')
-    {
-      document.getElementById("foodex").innerHTML += x ;
+    if (e == 'food') {
+        var s = Number(x) + Number(document.getElementById("foodex").innerHTML);
+        document.getElementById("foodex").innerHTML = s;
     }
-    if(e=='travel')
-    {
-      document.getElementById("travelex").innerHTML += x ;
+    if (e == 'travel') {
+        var s = Number(x) + Number(document.getElementById("travelex").innerHTML);
+        document.getElementById("travelex").innerHTML = s;
     }
-    if(e=='clothes')
-    {
-      document.getElementById("clothesex").innerHTML += x ;
+    if (e == 'clothes') {
+        var s = Number(x) + Number(document.getElementById("clothesex").innerHTML);
+        document.getElementById("clothesex").innerHTML = s;
     }
-    if(e=='bill')
-    {
-      document.getElementById("billex").innerHTML += x ;
+    if (e == 'bill') {
+        var s = Number(x) + Number(document.getElementById("billex").innerHTML);
+        document.getElementById("billex").innerHTML = s;
     }
+}
+
+function excess() {
+    if (document.getElementById("foodex").value > document.getElementById("fl").value) {
+        highlight("food", "fl", "fper", "foodex");
+    }
+    if (document.getElementById("travelex").value > document.getElementById("tl").value) {
+        highlight("travel", "tl", "tper", "travelex");
+    }
+    if (document.getElementById("clothesex").value > document.getElementById("cl").value) {
+        highlight("clothes", "cl", "cper", "clothesex");
+    }
+    if (document.getElementById("billex").value > document.getElementById("bl").value) {
+        highlight("bill", "bl", "bper", "billex");
+    }
+}
+
+function highlight(category, categoryl, categoryper, categoryex) {
+    document.getElementById(category).style.background = "red";
+    document.getElementById(categoryl).style.color = "red";
+    document.getElementById(categoryper).style.color = "red";
+    document.getElementById(categoryex).style.color = "red";
 }
