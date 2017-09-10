5fb628118802526608f8fad84260c0c83bd1e3b4
Abinash <abinashp437@gmail.com>
2017-09-10 19:33:46 +0530

    changed the clear function and the link

--- spellbug/index.js
+++ spellbug/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var commonwords = [];
-  $.getJSON('dictionary.json', function(data) {
+  $.getJSON('https://raw.githubusercontent.com/saisankargochhayat/gdg-prelims-2017/master/spellbug/dictionary.json', function(data) {
   commonwords = data.commonWords;
   console.log(commonwords);
   });
@@ -8,11 +8,11 @@ $(document).ready(function(){
     var x = document.getElementById("mytext").value;
     commonwords.push(x);
     document.getElementById("demo").innerHTML = x+" is added.";
-  }
-  console.log(commonwords);
-  clear = function(){
-  document.getElementById("demo").innerHTML = "";
-  check();  
+    console.log(commonwords);
+    }
+  clean = function(){
+    document.getElementById("demo").innerHTML = "";
+    check();
   }
   check = function(){
    var a = document.getElementById("mytext1").value;
